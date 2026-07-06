02f46db0471d89acec5f4942f52c80b235d59e56
judithpatrick13-boop <judithpatrick13@gmail.com>
2026-07-06 06:06:23 +0100

    Add judith-headshot.jpg to about page

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,18 @@
-Search and replace the SVG in the About section with img tag
+import Image from 'next/image';
+
+export default function About() {
+  return (
+    <div className="about-page">
+      <section className="about-section">
+        <Image
+          src="/images/about/judith-headshot.jpg"
+          alt="Judith headshot"
+          width={400}
+          height={500}
+          priority
+        />
+        {/* Add your about content here */}
+      </section>
+    </div>
+  );
+}
